8dec174a8b6c159dcc6b1762220a6f80232bbce7
Emmy <fajumoe@gmial.com>
2026-07-14 17:42:46 +0100

    removed confg

--- config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config.example.php
-// Copy this file to config.php and enter your local database settings.
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'kenestore');
-define('DB_USER', 'root');
-define('DB_PASS', '');
