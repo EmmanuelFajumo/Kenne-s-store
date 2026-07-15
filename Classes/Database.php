0f939e4f6aa4552b7aec78b1856c2b417501714b
Emmy <fajumoe@gmial.com>
2026-07-15 14:29:46 +0100

    db

--- Classes/Database.php
+++ Classes/Database.php
@@ -5,11 +5,6 @@ if (file_exists(__DIR__ . '/../config.php')) {
     require_once __DIR__ . '/../config.php';
 }
 
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if (!defined('DB_NAME')) define('DB_NAME', 'kenestore');
-if (!defined('DB_USER')) define('DB_USER', 'root');
-if (!defined('DB_PASS')) define('DB_PASS', '');
-
 class Database {
     private $host = DB_HOST;
     private $db_name = DB_NAME;
